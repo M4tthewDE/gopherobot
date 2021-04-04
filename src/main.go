418cc6d2ed6031d7bf68133b8e8997e116a79865
M4tthewDE <mattider2@gmail.com>
2021-04-04 16:45:41 +0200

    accepts messages via rpc

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gempir/go-twitch-irc/v2"
+	"net"
+	"net/rpc"
 	"os"
 	"strings"
 )
@@ -14,6 +16,8 @@ func main() {
 	client.OnPrivateMessage(onMessage)
 
 	client.Join("gopherobot", "matthewde", "turtoise")
+
+	go remoteMessageHandler(client)
 
 	err := client.Connect()
 	if err != nil {
@@ -36,6 +40,37 @@ func doCommand(message twitch.PrivateMessage) {
 		EchoCommand(message, client)
 	case "id":
 		UserIdCommand(message, client)
+	case "addfollowalert":
+		AddFollowAlertCommand(message, client)
+	case "removefollowalert":
+		RemoveFollowAlertCommand(message, client)
+	}
+}
+
+func remoteMessageHandler(client *twitch.Client) error {
+	addy, err := net.ResolveTCPAddr("tcp", "0.0.0.0:42586")
+	if err != nil {
+		return err
 	}
 
+	inbound, err := net.ListenTCP("tcp", addy)
+	if err != nil {
+		return err
+	}
+
+	messageHandler := new(MessageHandler)
+	rpc.Register(messageHandler)
+	rpc.Accept(inbound)
+
+	return nil
+}
+
+type MessageHandler int
+
+func (m *MessageHandler) SendMessage(content string, ack *bool) error {
+	channel := strings.Split(content, " ")[0]
+	message := content[len(channel):]
+
+	client.Say(channel, message)
+	return nil
 }
