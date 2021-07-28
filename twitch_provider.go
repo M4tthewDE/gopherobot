a7a47544f092e88cb17b88ceed18627a4328757f
M4tthewDE <mattider2@gmail.com>
2021-07-28 17:33:03 +0200

    remove unused struct

--- twitch_provider.go
+++ twitch_provider.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-
 	"github.com/nicklaw5/helix"
 )
 
@@ -38,19 +36,4 @@ func GetUser(id string) (string, error) {
 		return "", err
 	}
 	return resp.Data.Users[0].Login, nil
-}
-
-type GetUserIdJson struct {
-	Data []struct {
-		ID              string    `json:"id"`
-		Login           string    `json:"login"`
-		DisplayName     string    `json:"display_name"`
-		Type            string    `json:"type"`
-		BroadcasterType string    `json:"broadcaster_type"`
-		Description     string    `json:"description"`
-		ProfileImageURL string    `json:"profile_image_url"`
-		OfflineImageURL string    `json:"offline_image_url"`
-		ViewCount       int       `json:"view_count"`
-		CreatedAt       time.Time `json:"created_at"`
-	} `json:"data"`
 }
