f809a19bdde7b7d8bfc08968a6359011022d8cf4
M4tthewDE <mattider2@gmail.com>
2021-07-28 17:35:34 +0200

    Use nicklaw5 helix (#31)
    * move GetUserID to nicklaw5/helix
    
    * move GetUser to nicklaw5/helix
    
    * remove unused struct

--- commands.go
+++ commands.go
@@ -19,16 +19,17 @@ func UserIdCommand(message twitch.PrivateMessage) string {
 	if err != nil {
 		return `Couldn't find User-ID for "` + args[0] + `"`
 	} else {
-		return "User-ID of " + args[0] + " is " + strconv.Itoa(id)
+		return "User-ID of " + args[0] + " is " + id
 	}
 }
 
 func UserCommand(message twitch.PrivateMessage) string {
-	args := strings.Split(message.Message[6:], " ")
-	id, err := strconv.Atoi(args[0])
-	if err != nil {
-		return "Invalid User-ID " + args[0]
+	if len(message.Message) < 6 {
+		return `No ID provided`
 	}
+	args := strings.Split(message.Message[6:], " ")
+
+	id := args[0]
 
 	user, err := GetUser(id)
 	if err != nil {
@@ -77,7 +78,7 @@ func GetFollowAlertsCommand() string {
 
 	var users []string
 	for _, webhook := range followWebhook.Data {
-		id, _ := strconv.Atoi(webhook.Condition.BroadcasterUserID)
+		id := webhook.Condition.BroadcasterUserID
 		user, _ := GetUser(id)
 		users = append(users, user)
 	}
--- twitch_provider.go
+++ twitch_provider.go
@@ -1,110 +1,39 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"io"
-	"log"
-	"net/http"
-	"strconv"
-	"time"
+	"github.com/nicklaw5/helix"
 )
 
-func GetUserID(user string) (int, error) {
-	url := "https://api.twitch.tv/helix/users?login=" + user
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
+func GetUserID(user string) (string, error) {
+	client, err := helix.NewClient(&helix.Options{
+		ClientID:        Conf.Twitch.Client_ID,
+		UserAccessToken: Conf.Twitch.Token,
+	})
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-
-	req.Header.Add("Client-ID", Conf.Twitch.Client_ID)
-	req.Header.Add("Authorization", "Bearer "+Conf.Twitch.Token)
-
-	r, err := client.Do(req)
+	resp, err := client.GetUsers(&helix.UsersParams{
+		Logins: []string{user},
+	})
 	if err != nil {
-		return 0, err
+		return "", err
 	}
-	defer r.Body.Close()
-
-	data, err := io.ReadAll(r.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	if r.StatusCode != 200 {
-		return 0, errors.New(strconv.Itoa(r.StatusCode))
-	}
-
-	var getUserIdJson GetUserIdJson
-	err = json.Unmarshal(data, &getUserIdJson)
-	if err != nil {
-		return 0, err
-	}
-
-	if len(getUserIdJson.Data) == 0 {
-		return 0, errors.New("no User-ID found")
-	}
-
-	id, err := strconv.Atoi(getUserIdJson.Data[0].ID)
-	if err != nil {
-		return 0, err
-	}
-	return id, nil
+	return resp.Data.Users[0].ID, nil
 }
 
-func GetUser(id int) (string, error) {
-	url := "https://api.twitch.tv/helix/users?id=" + strconv.Itoa(id)
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("Client-ID", Conf.Twitch.Client_ID)
-	req.Header.Add("Authorization", "Bearer "+Conf.Twitch.Token)
-
-	r, err := client.Do(req)
+func GetUser(id string) (string, error) {
+	client, err := helix.NewClient(&helix.Options{
+		ClientID:        Conf.Twitch.Client_ID,
+		UserAccessToken: Conf.Twitch.Token,
+	})
 	if err != nil {
 		return "", err
 	}
-	defer r.Body.Close()
-
-	data, err := io.ReadAll(r.Body)
+	resp, err := client.GetUsers(&helix.UsersParams{
+		IDs: []string{id},
+	})
 	if err != nil {
 		return "", err
 	}
-
-	if r.StatusCode != 200 {
-		return "", errors.New(strconv.Itoa(r.StatusCode))
-	}
-
-	var getUserIdJson GetUserIdJson
-	err = json.Unmarshal(data, &getUserIdJson)
-	if err != nil {
-		return "", err
-	}
-
-	if len(getUserIdJson.Data) == 0 {
-		return "", errors.New("no User-ID found")
-	}
-
-	return getUserIdJson.Data[0].DisplayName, nil
-}
-
-type GetUserIdJson struct {
-	Data []struct {
-		ID              string    `json:"id"`
-		Login           string    `json:"login"`
-		DisplayName     string    `json:"display_name"`
-		Type            string    `json:"type"`
-		BroadcasterType string    `json:"broadcaster_type"`
-		Description     string    `json:"description"`
-		ProfileImageURL string    `json:"profile_image_url"`
-		OfflineImageURL string    `json:"offline_image_url"`
-		ViewCount       int       `json:"view_count"`
-		CreatedAt       time.Time `json:"created_at"`
-	} `json:"data"`
+	return resp.Data.Users[0].Login, nil
 }
