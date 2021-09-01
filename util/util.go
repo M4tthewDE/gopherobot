96aa4cc8d002cd15737401b28a858288e25575bc
M4tthewDE <mattider2@gmail.com>
2021-09-01 11:46:52 +0200

    fix util linting

--- util/util.go
+++ util/util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 	"strings"
 	"time"
@@ -11,29 +12,32 @@ import (
 
 func GetCommit() (string, error) {
 	cmd := exec.Command("git", "rev-parse", "--short", "HEAD")
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
-	err := cmd.Run()
-	if err != nil {
-		return "", err
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("error getting commit: %w", err)
 	}
+
 	return strings.TrimSuffix(out.String(), "\n"), nil
 }
 
 func GetBranch() (string, error) {
 	cmd := exec.Command("git", "branch", "--show-current")
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
-	err := cmd.Run()
-	if err != nil {
-		return "", err
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("error getting branch: %w", err)
 	}
+
 	return strings.TrimSuffix(out.String(), "\n"), nil
 }
 
 func GetLatency(message twitch.PrivateMessage) string {
 	latency := time.Since(message.Time)
+
 	return latency.String()
 }
