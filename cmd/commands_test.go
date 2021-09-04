12dbe750af7c382bd9fe04874e7ef1bebd2e904b
M4tthewDE <mattider2@gmail.com>
2021-09-04 21:17:31 +0200

    complete test for HTTPStatusCommand - adjust linter settings

--- cmd/commands_test.go
+++ cmd/commands_test.go
@@ -1,6 +1,7 @@
 package cmd_test
 
 import (
+	"fmt"
 	"testing"
 
 	"de.com.fdm/gopherobot/cmd"
@@ -12,20 +13,72 @@ func TestHTTPStatusCommand(t *testing.T) {
 	t.Parallel()
 
 	codes := map[string]string{
-		100, "Continue",
-		"101", "Switching Protocols",
-		"102", "Processing",
-		"200", "OK",
-		"201", "Created",
+		"100": "Continue",
+		"101": "Switching Protocols",
+		"102": "Processing",
+		"200": "OK",
+		"201": "Created",
+		"202": "Accepted",
+		"203": "Non-Authoritative Information",
+		"204": "No Content",
+		"206": "Partial Content",
+		"207": "Multi-Status",
+		"300": "Multiple Choices",
+		"301": "Moved Permanently",
+		"302": "Found",
+		"303": "See Other",
+		"304": "Not Modified",
+		"305": "Use Proxy",
+		"307": "Temporary Redirect",
+		"308": "Permanent Redirect",
+		"400": "Bad Request",
+		"401": "Unauthorized",
+		"402": "Payment Required",
+		"403": "Forbidden",
+		"404": "Not Found",
+		"405": "Method Not Allowed",
+		"406": "Not Acceptable",
+		"407": "Proxy Authentication Required",
+		"408": "Request Timeout",
+		"409": "Conflict",
+		"410": "Gone",
+		"411": "Length Required",
+		"412": "Precondition Failed",
+		"413": "Request Entity Too Large",
+		"414": "Request URI Too Long",
+		"415": "Unsupported Media Type",
+		"416": "Requested Range Not Satisfiable",
+		"417": "Expectation Failed",
+		"418": "I'm a teapot",
+		"421": "Misdirected Request",
+		"422": "Unprocessable Entity",
+		"423": "Locked",
+		"424": "Failed Dependency",
+		"425": "Too Early",
+		"426": "Upgrade Required",
+		"428": "Precondition Required",
+		"429": "Too Many Requests",
+		"431": "Request Header Fields Too Large",
+		"451": "Unavailable For Legal Reasons",
+		"500": "Internal Server Error",
+		"501": "Not Implemented",
+		"502": "Bad Gateway",
+		"503": "Service Unavailable",
+		"504": "Gateway Timeout",
+		"505": "HTTP Version Not Supported",
+		"506": "Variant Also Negotiates",
+		"507": "Insufficient Storage",
+		"508": "Loop Detected",
+		"510": "Not Extended",
+		"511": "Network Authentication Required",
 	}
 
 	cmdHandler := cmd.CommandHandler{}
-	message := twitch.PrivateMessage{
-		Message: ";httpstatus 100",
+	message := twitch.PrivateMessage{}
+
+	for status, expected := range codes {
+		message.Message = fmt.Sprintf(";httpstatus %s", status)
+		result := cmdHandler.HTTPStatusCommand(message)
+		assert.Equal(t, result, expected, "should be equals")
 	}
-
-	result := cmdHandler.HTTPStatusCommand(message)
-
-	assert.Equal(t, result, "Continue", "should be equals")
-
 }
