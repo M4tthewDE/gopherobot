fb53f48a24a7f48855f2ccdf6bcf4b473637c2ed
M4tthewDE <mattider2@gmail.com>
2022-10-08 15:02:24 +0200

    Add log flags to print line number and file

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"de.com.fdm/gopherobot/bot"
 	"de.com.fdm/gopherobot/config"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	config := config.GetConfig()
 
 	bot := bot.NewBot(config)
